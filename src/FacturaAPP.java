54dbdfca653afc1ee0cfdb09c9e7abbc4566ca91
Sergi Marques <smarques2@nigul.cide.es>
2022-12-13 18:04:26 +0100

    Arrays

--- src/FacturaAPP.java
+++
@@ -1,9 +0,0 @@
-public class FacturaAPP {
-    public static void main (String [] args){
-        Factura factura1=new Factura();
-
-        factura1.CalcularFactura(Factura);
-
-        System.out.println(factura1);
-    }
-}
